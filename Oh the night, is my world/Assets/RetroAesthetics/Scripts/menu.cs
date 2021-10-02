bf2b155b4caf5876efe3efdb42c579e1038bc438
Cosmofeline <ceydanurucans@gmail.com>
2021-10-02 23:43:04 +0300

    My Game

---
+++ Oh the night, is my world/Assets/RetroAesthetics/Scripts/menu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+public class menu : MonoBehaviour
+{
+  //fuck this life man
+    public void Play()
+    {
+         SceneManager.LoadScene("Game");
+    }
+}
